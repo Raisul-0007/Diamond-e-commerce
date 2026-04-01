6d3c8d60f7b4766f5c527f273f848adfadd83a16
Raisul-0007 <raisulislamriat007@gmail.com>
2026-04-01 14:30:15 +0600

    1 april

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navber from './components/Navber'
 
 const App = () => {
   return (
-    <div className=''>
-      ami
-    </div>
+    <>
+    <Navber/>
+    </>
   )
 }
 
