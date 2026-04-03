8a12d66f3fce6a438ec95d20a92c70999bbed511
Raisul-0007 <raisulislamriat007@gmail.com>
2026-04-03 23:21:50 +0600

    3 april 26

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navber from './components/Navber'
 import Container from './components/Container'
+import Banner from './components/Banner'
 
 const App = () => {
   return (
     <div>
     <Container>
     <Navber/>
+    <Banner/>
     </Container>
     </div>
   )
