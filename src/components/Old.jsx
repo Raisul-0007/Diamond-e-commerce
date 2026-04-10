fcb499e379d7934d09629b3177b81504b1e4176e
Raisul-0007 <raisulislamriat007@gmail.com>
2026-04-10 22:29:42 +0600

    old component creat

---
+++ src/components/Old.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Old = () => {
+  return (
+    <div className='py-15'>
+      <h2 className='text-5xl font-bold'>XIV<span className='block'>Collections</span><span className='block'>23-24</span></h2>
+    </div>
+  )
+}
+
+export default Old
