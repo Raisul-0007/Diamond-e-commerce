dfa9e71e5e2a20e69236d07c7a702bb1d4a3bbac
Raisul-0007 <raisulislamriat007@gmail.com>
2026-04-16 15:36:55 +0600

    16 april

--- src/components/Approach.jsx
+++ src/components/Approach.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Approach from './Approach';
 
 const Approach = () => {
   return (
     <div>
-      Approach
+      <div className="">
+        <p>at elegant vogue , we blend creativity with craftsmanship to create fashion that transcends trends and stands the test of time each design is meticulously crafted, ensuring the highest quelity exqulsite finish</p>
+      </div>
       <div className="">Ami</div>
     </div>
   )
