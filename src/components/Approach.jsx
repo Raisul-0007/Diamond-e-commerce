ea6899038e7faa0b7784ff93f3b950a8090224de
Raisul-0007 <raisulislamriat007@gmail.com>
2026-04-17 21:11:41 +0600

    17 april

--- src/components/Approach.jsx
+++ src/components/Approach.jsx
@@ -4,7 +4,7 @@ const Approach = () => {
   return (
     <div>
       <div className="">
-        <h2>Our Approach to fashion design </h2>
+        <h2 className="">Our Approach to fashion design </h2>
         <p>at elegant vogue , we blend creativity with craftsmanship to create fashion that transcends trends and stands the test of time each design is meticulously crafted, ensuring the highest quelity exqulsite finish</p>
       </div>
       <div className="">Ami</div>
