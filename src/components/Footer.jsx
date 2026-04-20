8dbb059b168280f0028c01cce9abd1cd22736e2e
Raisul-0007 <raisulislamriat007@gmail.com>
2026-04-20 18:26:51 +0600

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Container from './Container'
-
+import logo from "../assets/logo.png"
 const Footer = () => {
   return (
     <div className='bg-[#F5F5F5] py-30'>
-      <Container>
-        <div className="">
+      <Container className="flex">
+        <div className="w-1/2">
         <div className="">
           <p className='text-[#1e1e1e5f] py-10'>info</p>
         <div className="">
@@ -27,7 +27,21 @@ const Footer = () => {
         </div>
         </div>
       </div>
-      <div className=""></div>
+      <div className="w-1/2 ">
+       <p className='py-20 text-[#7676765f]'>Technologies</p>
+      <div className="flex items-center gap-20">
+        <div className="relative">
+          <h2 className='text-7xl text-[#76767612]'>VR
+            <span><img className='absolute top-5 left-0' src={logo} alt="" /></span>
+          </h2>
+          <h2 className='text-7xl'>XIV</h2>
+        <h2 className='text-7xl leading-20'>XQ</h2>
+        </div>
+        <div className="">
+          <p className='text-[#7676765f]'>Near-field communication </p>
+        </div>
+        </div>
+      </div>
       </Container>
     </div>
   )
