c44d050e946e120e72a072b93f08ccac27174d64
Raisul-0007 <raisulislamriat007@gmail.com>
2026-04-28 21:04:13 +0600

    Product Details Complete

--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { IoIosStar, IoIosStarHalf, IoIosStarOutline } from 'react-icons/io'
+import { Link, useParams } from 'react-router-dom'
 const ProductDetails = () => {
    window.scrollTo(0,0)
    let productsId = useParams()
@@ -13,24 +14,39 @@ const ProductDetails = () => {
         console.log(err)
     }
 }
+
+let rating = oneProduct?.rating || 0;
+
+let clientRating = Array.from({length: 5}, (_, index)=>{
+  let number = index + 0.5
+  return oneProduct.rating >= index + 1 ? (
+    <IoIosStar />
+  ) : oneProduct.rating >= number ? (<IoIosStarHalf/>)
+    :
+    (<IoIosStarOutline />)
+})
 useEffect(() => {
     singleProduct()
 }, [productsId.id])
   return (
-    <div className='flex'>
+    <div className='flex gap-5 py-20 '>
       <div className="w-1/2">
-        <img className='w-1/2 mx-auto bg-white border border-[#D7D7D7]' src={oneProduct.thumbnail} alt="" />
+        <img className='w-135 ml-auto bg-white border border-[#D7D7D7] bg-cover ' src={oneProduct.thumbnail} alt="" />
       </div>
       <div className="w-1/2">
-        <div className="w-120 px-10 py-20 border border-[#D7D7D7]">
-          <h2 className='text-2xl font-bold'>{oneProduct?.title?.slice(0, 20)}</h2>
-        <h3 className='text-xl font-semibold'>$ {oneProduct?.price?.toFixed(2)}</h3>
+        <div className="w-100 px-10 py-20 border shadow-xl border-[#D7D7D7]">
+          <div className="flex gap-0.5 py-3 text-[#ffcf00] text-lg">{clientRating}</div>
+          <h2 className='text-2xl font-bold '>{oneProduct?.title?.slice(0, 20)}</h2>
+        <h3 className='text-xl font-semibold text-[#262626e4]'>$ {oneProduct?.price?.toFixed(2)}</h3>
         <div className="py-2">
           <h4 className='text-[#767676]'>MRP incl. of all taxes</h4>
         </div>
-        <div className="py-10">
+        <div className="py-9.5">
           <p>{oneProduct?.description}</p>
         </div>
+        <div className="flex gap-5 justify-center">
+          <Link className='px-5 py-2 border border-[#262626] bg-[#262626] hover:bg-[#767676] rounded-[20px] text-white' to="">Add To Cart</Link>
+          <Link className='px-5 py-2 border border-[#262626] hover:bg-[#262626] rounded-[20px] hover:text-white' to="">Add To Favourite</Link>        </div>
         </div>
       </div>
     </div>
