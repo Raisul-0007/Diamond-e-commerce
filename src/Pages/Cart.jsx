c0f6df90d4d6c024c8a4a67b97b5075fb353fd84
Raisul-0007 <raisulislamriat007@gmail.com>
2026-05-02 21:36:51 +0600

    5 april

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -25,33 +25,47 @@ const Cart = () => {
         <div className="">
           <h1 className='text-5xl'>Add To Cart</h1>
         </div>
-      <div className="py-20">
-        {cartData.length > 0 ? 
-        (cartData.map((item)=>(
-        <div className="flex items-center border border-[#7676763e] rounded-2xl p-5">
-        <div className="w-2/3 flex items-center justify-between">
-        <div onClick={()=>handleRemove(item)} >
-            <div className="p-1 border border-[#7676763e] hover:bg-[#76767607] cursor-pointer"><RxCross2/></div>
+        <div className="">
+          <div className="flex justify-between">
+            <div className="w-2/4">
+              <h3>Product Details</h3>
+            </div>
+            <div className="w-1/4">
+              <h3>Price</h3>
+              <h3>Quantity</h3>
+            </div>            
+            <div className="w-1/4">
+              <h3>Total</h3>
+            </div>
           </div>
           <div className="">
-        <img className='h-20 w-20 bg-white border border-[#7676763e]' src={item.thumbnail} alt="" />
-        </div>
-        <div className="">
-          <h3>{item.title}</h3>
-        </div>
-         <div className="flex items-center gap-3">
-          <button onClick={()=>handlePlus(item)} className="p-1 border border-[#7676763e] hover:bg-[#76767607] cursor-pointer"><FiPlus/></button>
-          <div className="">{item.qun}</div>
-          <button onClick={()=>handleMinus(item)} className="p-1 border border-[#7676763e] hover:bg-[#76767607] cursor-pointer"><FiMinus/></button>
-        </div>
-        </div>
-        <div className="w-1/3 flex items-center gap-20">
-          <h5>${item.price}</h5>
-          <h6>${item.qun * item.price}</h6>
-        </div>
-        </div>
-      ))):
-      <div className=""></div> }
+              {cartData.length > 0 ? 
+              (cartData.map((item)=>(
+              <div className="flex items-center justify-between border border-[#7676763e] rounded-2xl p-5">
+              <div className='w-2/4 flex items-center gap-10' onClick={()=>handleRemove(item)} >
+                  <div className="flex items-center justify-center h-6 w-6 border border-[#7676763e] hover:bg-[#76767607] cursor-pointer"><RxCross2/></div>
+                  <div className="">
+              <img className='h-20 w-20 bg-white border border-[#7676763e]' src={item.thumbnail} alt="" />
+              </div>
+              <div className="">
+                <h3>{item.title}</h3>
+              </div>
+                </div>
+              <div className="w-1/4 flex justify-between">
+                <h5>${item.price}</h5>
+                <div className="flex items-center gap-3">
+                  <button onClick={()=>handleMinus(item)} className="p-1 border border-[#7676763e] hover:bg-[#76767607] cursor-pointer"><FiMinus/></button>
+                <div className="">{item.qun}</div>
+                <button onClick={()=>handlePlus(item)} className="p-1 border border-[#7676763e] hover:bg-[#76767607] cursor-pointer"><FiPlus/></button>
+              </div>
+              </div>
+              <div className="w-1/4 flex items-center justify-center gap-20">
+                <h6>${item.qun * item.price}</h6>
+              </div>
+              </div>
+            ))):
+            <div className=""></div> }
+            </div>
       </div>
       </Container>
     </div>
