4fb723c26c5f243febe5e3fef5f49c9b94942bdd
Raisul-0007 <raisulislamriat007@gmail.com>
2026-07-24 07:49:24 +0600

    Products responsive

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -29,14 +29,14 @@ const Products = ({active, allPage}) => {
         :
         <div className="flex flex-wrap">
             {allPage?.map ((item)=>(
-           <div key={item.id} className="lg:w-1/3 px-5">
+           <div key={item.id} className="lg:w-1/3 w-1/2 lg:px-5 px-1">
                 <Link className='p-4' to={`/Shop/${item.id}`}>
-        <img src={item.thumbnail} alt="" className=" lg:h-90 h-70  object-cover bg-white border border-[#D7D7D7]"/>
+        <img src={item.thumbnail} alt="" className=" lg:h-90  object-cover h-40 bg-white border border-[#D7D7D7]"/>
 
-          <p className="text-[#00000067] pt-2 lg:text-[16px] capitalize text-sm">{item.category.slice(0, 10)}</p>
-        <div className="flex justify-between items-center ">
-          <h3 className="lg:text-lg text-[16px] font-semibold ">{item.title.slice(0, 10)}</h3>
-          <p className="font-semibold pr-2 lg:text-[16px] text-[14px]">${item.price}</p>
+          <p className="text-[#00000067] pt-2 lg:text-[16px] capitalize text-[10px]">{item.category.slice(0, 10)}</p>
+        <div className="flex justify-between items-center gap-1">
+          <h3 className="lg:text-lg text-[12px] font-semibold ">{item.title.slice(0, 10)}</h3>
+          <p className="font-semibold pr-2 lg:text-[16px] text-[10px]">${item.price}</p>
         </div>
     </Link>
            </div> 
